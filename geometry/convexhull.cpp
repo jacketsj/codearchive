5414e8e2bc841fa15b1e595548f57b185a32112e
jacketsj <jacketsj@gmail.com>
2018-02-05 12:05:22 -0800

    geometry: Add graham scan

--- geometry/convexhull.cpp
+++ geometry/convexhull.cpp
@@ -7,6 +7,14 @@ struct pt
 	double x, y;
 	pt() {}
 	pt(double x, double y) : x(x), y(y) {}
+	pt operator+(const pt &o) const
+	{
+		return pt(x+o.x,y+o.y);
+	}
+	pt operator-(const pt &o) const
+	{
+		return pt(x-o.x,y-o.y);
+	}
 };
 
 pt basis;
@@ -22,13 +30,15 @@ bool ang_compare(const pt &a, const pt &b)
 		return false;
 	if (det+eps < 0)
 		return true;
-	//colinear
+	//colinear, can return false if colinearity not wanted
 	return p2(basis.x-a.x)+p2(basis.y-a.y) < p2(basis.x-b.x)+p2(basis.y-b.y);
 }
 
 vector<int> giftwrap(const vector<pt> &points)
 {
 	int n = points.size();
+	if (n == 0)
+		return vector<int>();
 	int bot = 0, boty = points[0].y;
 	for (int i = 1; i < n; ++i)
 	{
@@ -65,8 +75,11 @@ vector<int> giftwrap(const vector<pt> &points)
 	return ret;
 }
 
-vector<int> grahamscan(vector<pt> points)
+const vector<pt> *gsp;
+vector<int> grahamscan(const vector<pt> &points)
 {
+	gsp = &points;
+
 	int n = points.size();
 	if (n == 0)
 		return vector<int>();
@@ -79,8 +92,43 @@ vector<int> grahamscan(vector<pt> points)
 			bot = i; boty = p.y;
 		}
 	}
+
 	basis = points[bot];
-	return vector<int>();
+	vector<int> points_s; points_s.resize(n);
+	for (int i = 0; i < n; ++i)
+		if (i != bot)
+			points_s.push_back(i);
+	sort(points_s.begin(), points_s.end(),
+		[](int a, int b){return ang_compare((*gsp)[a], (*gsp)[b]);} );
+	points_s.push_back(bot);
+	stack<int> rets;
+	rets.push(bot);
+	for (int cur : points_s)
+	{
+		if (rets.size() < 2 && cur != bot)
+			rets.push(cur);
+		else if (rets.size() >= 2)
+		{
+			int t1, t2;
+			do
+			{
+				t1 = rets.top(); rets.pop(); t2 = rets.top();
+				basis = points[t1];
+			} while (rets.size() > 1 && !ang_compare(points[t1]+(points[t1]-points[t2]), points[cur])); //pops previous colinear points
+			if (rets.size() > 1 || ang_compare(points[t1]+(points[t1]-points[t2]), points[cur]))
+				rets.push(t1);
+			if (cur != bot)
+				rets.push(cur);
+		}
+	}
+
+	vector<int> ret;
+	while (!rets.empty())
+	{
+		ret.push_back(rets.top());
+		rets.pop();
+	}
+	return ret;
 }
 
 //usage
@@ -99,8 +147,16 @@ int main()
 	points.push_back(pt(2,1));
 	points.push_back(pt(3,1));
 
+	//giftwrap
 	vector<int> ch = giftwrap(points);
 	cout << "Giftwrap gives convex hull of: ";
+	for (int k : ch)
+		cout << '(' << points[k].x << ',' << points[k].y << "),";
+	cout << '\n';
+
+	//grahamscan
+	ch = grahamscan(points);
+	cout << "Graham scan gives convex hull of: ";
 	for (int k : ch)
 		cout << '(' << points[k].x << ',' << points[k].y << "),";
 	cout << '\n';
