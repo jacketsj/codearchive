c392b61d99825d4450c192a98ba2b04acbb0ef49
jacketsj <jacketsj@gmail.com>
2018-08-06 11:43:05 -0700

    Alter, update, and further generalize Dijkstra

--- graph/traversal.cpp
+++ graph/traversal.cpp
@@ -67,41 +67,66 @@ void bfs(int start, const vector<vector<int>> &adj, void (*visit)(int))
 	}
 }
 
-struct d_compare
+struct delem
 {
-	bool operator()(const pair<int,int> &a, const pair<int,int> &b)
+	int pos, dist, origin;
+	delem(int p, int d, int o) : pos(p), dist(d), origin(o) {}
+	bool operator<(const delem &oth) const
 	{
-		return a.second < b.second;
+		//priority_queue is a max heap
+		return oth.dist < dist;
 	}
 };
-vector<int> dijkstra(int start, const vector<vector<int>> &adj, const vector<vector<int>> &pri, void (*visit)(int))
+vector<int> dijkstra(int start, const vector<vector<int>> &adj, const vector<vector<int>> &pri, void (*visit)(delem))
 {
 	int n = adj.size();
-	vector<bool> visited; visited.resize(n);
-	vector<int> dist; dist.resize(n);
-	priority_queue<pair<int,int>,vector<pair<int,int>>,d_compare> to_visit;
-	to_visit.push(pair<int,int>(start,0));
+	vector<bool> visited(n);
+	vector<int> dist(n);
+	for (int i = 0; i < n; ++i)
+		dist[i] = -1; //infinity
+	priority_queue<delem> to_visit;
+	to_visit.push(delem(start, 0, -1));
 	while (!to_visit.empty())
 	{
-		pair<int,int> k = to_visit.top(); to_visit.pop();
-		int k0 = k.first;
-		if (visited[k0])
-		{
-			dist[k0] = min(dist[k0],k.second);
+		delem cur = to_visit.top(); to_visit.pop();
+		if (visited[cur.pos])
 			continue;
-		}
-		visited[k0] = true;
-		dist[k0] = k.second;
-		visit(k0);
-
-		for (int i = 0; i < adj[k0].size(); ++i)
-			to_visit.push(pair<int,int>(adj[k0][i],pri[k0][i]+k.second));
+		visited[cur.pos] = true;
+		dist[cur.pos] = cur.dist;
+		visit(cur);
+		for (int i = 0; i < adj[cur.pos].size(); ++i)
+			to_visit.push(delem(adj[cur.pos][i], cur.dist+pri[cur.pos][i], cur.pos));
 	}
 	return dist;
 }
-vector<int> dijkstra2(int start, const vector<vector<int>> &adj, const vector<vector<int>> &pri)
+vector<int> dijkstra2(int start, const vector<vector<int>> &adj, const vector<vector<int>> &pri, void (*visit)(int))
 {
-	return dijkstra(start, adj, pri, [](int i) {});
+	static void (*visitst)(int) = [](int i) {};
+	visitst = visit;
+	return dijkstra(start, adj, pri, [](delem d) { visitst(d.pos); });
+}
+vector<int> dijkstra3(int start, const vector<vector<int>> &adj, const vector<vector<int>> &pri)
+{
+	return dijkstra2(start, adj, pri, [](int i) {});
+}
+vector<int> dijkstra4(int start, int end, const vector<vector<int>> &adj, const vector<vector<int>> &pri)
+{
+	static vector<int> *prevst;
+	vector<int> prev(adj.size());
+	prevst = &prev;
+	vector<int> dist = dijkstra(start, adj, pri, [](delem d) { (*prevst)[d.pos] = d.origin; });
+	vector<int> path;
+	if (dist[end] == -1) //no usable path
+		return path;
+	int cur = end;
+	path.push_back(cur);
+	while (cur != start)
+	{
+		cur = prev[cur];
+		path.push_back(cur);
+	}
+	reverse(path.begin(), path.end());
+	return path;
 }
 
 //usage
@@ -146,10 +171,15 @@ int main()
 	adj2[0].push_back(3); pri2[0].push_back(4); //e.g. slower (but fewer edge) path 0->4
 	adj2[3].push_back(4); pri2[3].push_back(5); //end e.g.
 	adj2[2].push_back(0); pri2[2].push_back(0); //cycle-inducing edge
-	vector<int> dist = dijkstra2(0, adj2, pri2);
+	vector<int> dist = dijkstra3(0, adj2, pri2);
 	cout << "Dijstra:\n";
 	for (int i = 0; i < n2; ++i)
 	{
 		cout << "    dist from 0 to " << i << " = " << dist[i] << '\n';
 	}
+	vector<int> path = dijkstra4(0, 4, adj2, pri2);
+	cout << "Path from 0 to 4: ";
+	for (auto i : path)
+		cout << i << ' ';
+	cout << '\n';
 }
